cceed3057a04519ce51872f5485368230e63ac2b
gewl <gilbertwlawson@gmail.com>
2018-06-12 19:58:41 -0400

    first commit

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    int boardWidth = 5;
+    Transform[,] boardSpaces;
+
+    private void Awake()
+    {
+        boardSpaces = GetSpaces();
+    }
+
+    Transform[,] GetSpaces()
+    {
+        Transform[,] spaces = new Transform[boardWidth, boardWidth];
+
+        int childCount = transform.childCount;
+
+        int xCounter = 0, yCounter = 0; 
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform space = transform.GetChild(i);
+
+            spaces[xCounter, yCounter] = space;
+
+            xCounter++;
+
+            if (xCounter >= boardWidth)
+            {
+                yCounter++;
+                xCounter = 0;
+            }
+        }
+
+        return spaces;
+    }
+}
---
+++ Assets/Scripts/GlobalConstants.cs
@@ -0,0 +1,5 @@
+﻿public enum SpaceContents {
+    None,
+    Player,
+    Enemy
+}
