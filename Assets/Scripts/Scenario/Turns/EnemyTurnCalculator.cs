bcd19235a0bbf47346e96b6d5bb1299790cd69c3
gewl <gilbertwlawson@gmail.com>
2018-09-04 17:19:58 -0400

    clearing dead comments, adding stagnation avoidance to mobs (actually this time)

--- Assets/Scripts/Scenario/Turns/EnemyTurnCalculator.cs
+++ Assets/Scripts/Scenario/Turns/EnemyTurnCalculator.cs
@@ -39,11 +39,7 @@ public class EnemyTurnCalculator : MonoBehaviour {
             int enemyAttackRange = enemyAttackCard.range;
 
             List<EntityTurnTargets> possibleEntityTurns = enemyTile.GetAllPossibleEntityTurns(enemyMoveRange, enemyAttackRange);
-            List<EntityTurnTargets> sortedPotentialTurns = SortTurnTargetsByValue(enemyTile, possibleEntityTurns);
-            //List<Tile> potentialMoveTargetTiles = enemyTile.GetAllTilesWithinRange(enemyMoveRange);
-
-            //List<Tile> sortedPotentialMoveTargets = SortTilesByMoveEligibility(enemyTile, potentialMoveTargetTiles, enemyAttackRange);
-
+            List<EntityTurnTargets> sortedPotentialTurns = SortTurnTargetsByValue(enemyTile, possibleEntityTurns, gameState);
             int turnIndex = 0;
             EntityTurnTargets selectedTurnTargets = sortedPotentialTurns[turnIndex];
             List<Direction> movesToTargetMovementTile = BoardHelperFunctions.FindPathBetweenTiles(enemyTile, selectedTurnTargets.targetMovementTile);
@@ -66,19 +62,6 @@ public class EnemyTurnCalculator : MonoBehaviour {
             }
 
             upcomingEntityTargets.Add(selectedTurnTargets);
-            //projectedEnemyMovementTiles.Add(targetMovementTile);
-
-            //// IF enemy is projected to move into player's tile:
-            //// THEN attack as if enemy is NOT moving (to account for projected player displacement)
-            //// ELSE attack from projected movement tile
-            //Tile projectedMovementTile = targetMovementTile.DistanceFromPlayer == 0 ? enemyTile : targetMovementTile;
-
-            //List<Tile> potentialAttackTargetTiles = projectedMovementTile.GetTilesWithinLinearRange(enemyAttackRange);
-            //List<Tile> sortedPotentialAttackTargets = SortTilesByAttackEligibility(potentialAttackTargetTiles);
-            //Tile targetAttackTile = sortedPotentialAttackTargets[0];
-
-            //projectedEnemyAttackTiles.Add(targetAttackTile);
-
             int rangeOfProjectedAttack = BoardHelperFunctions.GetLinearDistanceBetweenTiles(selectedTurnTargets.targetMovementTile, selectedTurnTargets.targetAttackTile);
             Direction attackDirection = BoardHelperFunctions.GetDirectionFromPosition(selectedTurnTargets.targetMovementTile.Position, selectedTurnTargets.targetAttackTile.Position);
 
@@ -90,16 +73,16 @@ public class EnemyTurnCalculator : MonoBehaviour {
         }
     }
 
-    List<EntityTurnTargets> SortTurnTargetsByValue(Tile startingTile, List<EntityTurnTargets> unsortedList)
+    List<EntityTurnTargets> SortTurnTargetsByValue(Tile startingTile, List<EntityTurnTargets> unsortedList, ScenarioState expectedState)
     {
         return unsortedList
-            .OrderBy(turn => CalculateTurnValue(startingTile, turn))
+            .OrderBy(turn => CalculateTurnValue(startingTile, turn, expectedState))
             .ThenBy(tile => Random.Range(0f, 1f))
             .ToList();
     }
 
     // Used for OrderBy, so higher result = lower priority = lower value turn
-    int CalculateTurnValue(Tile startingTile, EntityTurnTargets turn)
+    int CalculateTurnValue(Tile startingTile, EntityTurnTargets turn, ScenarioState expectedState)
     {
         int result = 0;
 
@@ -107,58 +90,11 @@ public class EnemyTurnCalculator : MonoBehaviour {
         result += Mathf.Abs(startingTile.Position.y - turn.targetMovementTile.Position.y) * 2;
         result += (turn.targetAttackTile.DistanceFromPlayer * 5);
         result += upcomingEntityTargets.Any(target => target.targetAttackTile == turn.targetAttackTile) ? 15 : 0;
-        result += upcomingEntityTargets.Any(target => target.targetMovementTile == turn.targetAttackTile) ? 20 : 0;
+        result += upcomingEntityTargets.Any(target => expectedState.threatenedStagnationPositions.Contains(target.targetMovementTile.Position)) ? 20 : 0;
+        result += upcomingEntityTargets.Any(target => target.targetMovementTile == turn.targetAttackTile) ? 30 : 0;
+        result += upcomingEntityTargets.Any(target => expectedState.stagnatedPositions.Contains(target.targetMovementTile.Position)) ? 40 : 0;
 
         return result;
     }
 
-
-    //List<Tile> SortTilesByMoveEligibility(Tile startingTile, List<Tile> unsortedList, int attackRange)
-    //{
-    //    return unsortedList
-    //        .OrderBy(tile => CalculateTileMovementValue(startingTile, tile, attackRange))
-    //        .ThenBy(tile => Random.Range(0f, 1f))
-    //        .ToList<Tile>();
-    //}
-
-    // OrderBy is ascending, so higher result = lower value
-    //int CalculateTileMovementValue(Tile startingTile, Tile destinationTile, int attackRange)
-    //{
-    //    int result = 0;
-
-    //    result += Mathf.Abs(destinationTile.DistanceFromPlayer - attackRange) * 10;
-
-    //    // Super rough addition to increase value of tiles closer to origin tile
-    //    // without having to perform redundant path-mapping just to find exact distance.
-
-    //    result += Mathf.Abs(startingTile.Position.x - destinationTile.Position.x);
-    //    result += Mathf.Abs(startingTile.Position.y - destinationTile.Position.y);
-
-    //    result += destinationTile.DistanceFromPlayer == 0 ? 100 : 0;
-    //    result += projectedEnemyAttackTiles.Contains(destinationTile) ? 30 : 0;
-    //    result += projectedEnemyMovementTiles.Contains(destinationTile) ? 50 : 0;
-    //    result += currentlyOccupiedTiles.Contains(destinationTile) ? 100 : 0;
-
-    //    return result;
-    //}
-
-    //List<Tile> SortTilesByAttackEligibility(List<Tile> unsortedList)
-    //{
-    //    return unsortedList
-    //        .OrderBy(tile => CalculateTileAttackValue(tile))
-    //        .ThenBy(tile => Random.Range(0f, 1f))
-    //        .ToList<Tile>();
-    //}
-
-    //    int CalculateTileAttackValue(Tile tile)
-    //    {
-    //        int result = 0;
-
-    //        result += tile.DistanceFromPlayer;
-    //        result += projectedEnemyAttackTiles.Contains(tile) ? 3 : 0;
-    //        result += projectedEnemyMovementTiles.Contains(tile) ? 5 : 0;
-    //        result += currentlyOccupiedTiles.Contains(tile) ? 10 : 0;
-
-    //        return result;
-    //    }
 }
